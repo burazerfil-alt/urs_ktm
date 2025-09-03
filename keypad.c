8aeca9c9cf6a82b9d43844148c83998348d5e8de
burazerfil-alt <burazer.fil@gmail.com>
2025-09-03 22:52:54 +0200

    Update keypad.c

--- keypad.c
+++ keypad.c
@@ -1,6 +1,6 @@
-#include "keypad.h"   // Uključujemo header fajl s deklaracijama i HAL funkcijama
+#include "keypad.h"   // Uključujemo header file s deklaracijama i HAL funkcijama
 
-// --- Definicija pinova za redove i kolone tipkovnice ---
+// - Definicija pinova za redove i kolone tipkovnice 
 // Tipkovnica 4x4 spojena na GPIOA
 // Redovi: PA0, PA1, PA8, PA9
 // Kolone: PA4, PA5, PA6, PA7
@@ -8,7 +8,7 @@ static const uint16_t rowPins[4] = {GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_8, GPIO_PIN
 static const uint16_t colPins[4] = {GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7};
 #define KEYPAD_GPIO GPIOA   // cijela tipkovnica je na portu A
 
-// --- Matrica tipkovnice 4x4 ---
+// - Matrica tipkovnice 4x4 
 // Mapira kombinaciju (red, kolona) na određeni znak
 static const char keymap[4][4] = {
     {'1', '2', '3', 'A'},
@@ -17,14 +17,14 @@ static const char keymap[4][4] = {
     {'*', '0', '#', 'D'}
 };
 
-// --- Inicijalizacija tipkovnice ---
+// - Inicijalizacija tipkovnice 
 // (GPIO pinovi se podešavaju u CubeMX-u kao izlazi/ulazi, pa ovdje ništa ne radimo)
 void Keypad_Init(void) {
     // Ova funkcija ostaje prazna jer su pinovi konfigurirani u MX_GPIO_Init().
     // Ako želiš ručno, ovdje bi koristio HAL_GPIO_Init() i podesio row kao output, col kao input.
 }
 
-// --- Čitanje pritisnute tipke ---
+// - Čitanje pritisnute tipke 
 // Vraća znak tipke ili 0 ako ništa nije pritisnuto
 char Keypad_GetKey(void) {
     // Prolazimo kroz sve redove jedan po jedan
@@ -55,3 +55,4 @@ char Keypad_GetKey(void) {
     // Ako nijedna tipka nije pritisnuta → vrati 0
     return 0;
 }
+
